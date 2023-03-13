b4ef4b8c26e1bf6c2b6a93edc46e467bb0812397
Borusov Illia <113841816+borisovvilyaa@users.noreply.github.com>
2023-03-13 20:36:21 +0200

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,16 +1,36 @@
-cd out
-del out.exe
-cd ../
-cd obj/
-del obj/out.obj
-cd ../
-nasm -f win32 .\%~n1.asm -o obj/out.obj
+@echo off
+
+set ASSEMBLY_FILE=%~n1.asm
+set OBJ_FILE=obj\out.obj
+set EXE_FILE=out\out.exe
+
+echo Assembling %ASSEMBLY_FILE%...
+nasm -f win32 %ASSEMBLY_FILE% -o %OBJ_FILE%
+if errorlevel 1 (
+    echo ERROR: Assembling failed with error code %errorlevel%.
+    echo Check for syntax errors or invalid directives in %ASSEMBLY_FILE%.
+    exit /b %errorlevel%
+)
+echo Assembled %ASSEMBLY_FILE% to %OBJ_FILE%.
+
+echo Linking %OBJ_FILE% with required libraries...
+golink /entry:Start /console kernel32.dll Shell32.dll fpu.dll user32.dll %OBJ_FILE%
+if errorlevel 1 (
+    echo ERROR: Linking failed with error code %errorlevel%.
+    echo Check for missing libraries or unresolved symbols.
+    exit /b %errorlevel%
+)
+echo Linked %OBJ_FILE% with required libraries.
+
+echo Moving executable to output directory...
 cd obj
+move "out.exe" "../out/out.exe"
+cd ../
+echo Moved out.exe to %EXE_FILE%.
 
-golink /entry:Start /console kernel32.dll user32.dll out.obj 
-move "out.exe" "../out/out.exe" 
-
-cd ../out
-
+echo Running %EXE_FILE%...
+cd out
+echo --------------------------------------------------------------
+echo Output application: %EXE_FILE%
+echo --------------------------------------------------------------
 .\out.exe
-
